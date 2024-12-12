80b64f935314e92eadc8b87b12eaf6c952f039e4
samme <samme@users.noreply.github.com>
2024-12-12 07:43:13 -0800

    simplify package test

--- package.test.js
+++ package.test.js
@@ -3,26 +3,18 @@ import path from 'node:path'
 import { describe, expect, test } from 'vitest'
 import packageJson from './package.json'
 
-const validatePath = (relativePath) => {
-  const absolutePath = path.resolve(__dirname, relativePath)
-  return fs.existsSync(absolutePath)
-}
-
 describe('package.json paths', () => {
   test('main field should have a valid path', () => {
-    const mainPath = packageJson.main
-    expect(validatePath(mainPath)).toBe(true)
+    expect(fs.existsSync(packageJson.main)).toBe(true)
   })
 
   test('module field should have a valid path', () => {
-    const modulePath = packageJson.module
-    expect(validatePath(modulePath)).toBe(true)
+    expect(fs.existsSync(packageJson.module)).toBe(true)
   })
 
   test('files field should have valid paths', () => {
-    const files = packageJson.files
-    for (const file of files) {
-      expect(validatePath(file)).toBe(true)
+    for (const file of packageJson.files) {
+      expect(fs.existsSync(file)).toBe(true)
     }
   })
 })
